a6045d77f21ec0ee3d571178df9bc31c376f878a
Leticia-Mendes <mendesz.leticia@gmail.com>
2022-02-13 12:07:11 -0300

    poo 2

---
+++ 02POO/06_Dolar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace _06_Dolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cotação Dolar x Real!");
+            Console.Write("Qual é a cotação do dolar? ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.Write("Quantos dolares você quer comprar? ");
+            double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = ConversorDeMoedas.DolarParaReal(dolar, quantidade);
+
+            Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+        
+        }
+    }
+}

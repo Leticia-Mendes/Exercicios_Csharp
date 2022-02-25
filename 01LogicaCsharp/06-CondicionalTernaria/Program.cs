c6718beb258b1d4f4517831ec35953a0dcf0d931
Leticia-Mendes <mendesz.leticia@gmail.com>
2022-02-25 07:24:08 -0300

    exercicios

---
+++ 01LogicaCsharp/06-CondicionalTernaria/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace _06_CondicionalTernaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // (condição) ? valor_se_verdadeiro : valor_se_falso
+
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double desconto = (preco < 20) ? preco * 0.1 : preco * 0.2;
+            double total = preco - desconto;
+
+            Console.WriteLine("Desconto: R$" + desconto);
+            Console.WriteLine("Total: R$" + total);
+        }
+    }
+}

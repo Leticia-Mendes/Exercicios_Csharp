5b050c74691007d263e19a1cf4644456ae36c3fc
Leticia-Mendes <mendesz.leticia@gmail.com>
2022-02-27 11:36:40 -0300

    exercicios

--- 01LogicaCsharp/09-TimeSpan/Program.cs
+++ 01LogicaCsharp/09-TimeSpan/Program.cs
@@ -6,7 +6,41 @@ namespace _09_TimeSpan
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TimeSpan t1 = new TimeSpan(0, 1, 30);     // 00:01:30
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t1.Ticks);
+
+
+            TimeSpan t2 = new TimeSpan();    //hora 00:00:00
+            TimeSpan t3 = new TimeSpan(900000000L); // 00:01:30 em ticks
+            TimeSpan t4 = new TimeSpan(1, 11, 21);
+            TimeSpan t5 = new TimeSpan(1, 2, 11, 21);
+            TimeSpan t6 = new TimeSpan(1, 2, 11, 21, 231);
+
+            Console.WriteLine();
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine(t4);
+            Console.WriteLine(t5);
+            Console.WriteLine(t6);
+
+
+            TimeSpan t7 = TimeSpan.FromDays(1.5);
+            TimeSpan t8 = TimeSpan.FromHours(1.5);
+            TimeSpan t9 = TimeSpan.FromMinutes(1.5);
+            TimeSpan t10 = TimeSpan.FromSeconds(1.5);
+            TimeSpan t11 = TimeSpan.FromMilliseconds(1);
+            TimeSpan t12 = TimeSpan.FromTicks(900000000L);
+
+            Console.WriteLine();
+            Console.WriteLine(t7);
+            Console.WriteLine(t8);
+            Console.WriteLine(t9);
+            Console.WriteLine(t10);
+            Console.WriteLine(t11);
+            Console.WriteLine(t12);
+
         }
     }
 }
---
+++ 01LogicaCsharp/10-PropDateTime/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _10_PropDateTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d = new DateTime(2022, 2, 26, 17, 43, 58, 275);
+
+            Console.WriteLine(d);
+            Console.WriteLine("1) Date: " + d.Date);
+            Console.WriteLine("2) Day: " + d.Day);
+            Console.WriteLine("3) DayOfWeek: " + d.DayOfWeek);
+            Console.WriteLine("4) DayOfYear: " + d.DayOfYear);
+            Console.WriteLine("5) Hour: " + d.Hour);
+            Console.WriteLine("6) Kind: " + d.Kind);
+            Console.WriteLine("7) Millisecond: " + d.Millisecond);
+            Console.WriteLine("8) Minute: " + d.Minute);
+            Console.WriteLine("9) Month: " + d.Month);
+            Console.WriteLine("10) Second: " + d.Second);
+            Console.WriteLine("11) Ticks: " + d.Ticks);
+            Console.WriteLine("12) TimeOfDay: " + d.TimeOfDay);
+            Console.WriteLine("13) Year: " + d.Year);
+
+
+            string s1 = d.ToLongDateString();
+            string s2 = d.ToLongTimeString();
+            string s3 = d.ToShortDateString();
+            string s4 = d.ToShortTimeString();
+            string s5 = d.ToString();
+            string s6 = d.ToString("yyyy-MM-dd HH:mm:ss");
+            string s7 = d.ToString("yyyy-MM-dd HH:mm:ss.fff");
+
+            Console.WriteLine();
+            Console.WriteLine(d.ToLongDateString());
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
+            Console.WriteLine(s5);
+            Console.WriteLine(s6);
+            Console.WriteLine(s7);
+
+            DateTime x = new DateTime(2022, 2, 26, 17, 54, 33);
+
+            DateTime y = x.AddDays(2);
+            //DateTime y = x.AddHours(2);
+            //DateTime y = x.AddMilliseconds(33);
+            //DateTime y = x.AddMinutes(3);
+            //DateTime y = x.AddMonths(1);
+            //DateTime y = x.AddSeconds(3);
+            //DateTime y = x.AddTicks(900000000);
+            //DateTime y = x.AddYears(1);
+
+            TimeSpan t = x.Subtract(d);
+
+            Console.WriteLine();
+            Console.WriteLine(y);
+            Console.WriteLine(t);
+        }
+    }
+}

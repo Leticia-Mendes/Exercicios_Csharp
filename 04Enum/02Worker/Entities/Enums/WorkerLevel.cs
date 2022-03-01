09c56cd1bd94f1b309e661c8301d1198a9752fea
Leticia-Mendes <mendesz.leticia@gmail.com>
2022-03-01 15:45:23 -0300

    exercicios

---
+++ 04Enum/02Worker/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace _02Worker.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
---
+++ 04Enum/04Enum/Program.cs
@@ -0,0 +1,30 @@
+﻿using _04Enum.Entities;
+using _04Enum.Entities.Enums;
+using System;
+
+namespace _04Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+
+            string txt = OrderStatus.PendingPayment.ToString(); //Converter enum para string
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");  //Converter string para enum
+            Console.WriteLine(os);
+
+
+        }
+    }
+}

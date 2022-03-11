5dcc9f3be7934dca0c2df7b0cdab0a2b87af6987
Leticia-Mendes <mendesz.leticia@gmail.com>
2022-03-11 10:49:49 -0300

    exercicio herança e polimorfismo

---
+++ 05Heranca/Employee/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,20 @@
+﻿namespace PaymentEmployee.Entities
+{
+    internal class OutsourcedEmployee : Employee
+    {
+        public double AdicionalCharge { get; set; }
+
+        public OutsourcedEmployee() { }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double adicionalCharge) 
+            : base(name, hours, valuePerHour)
+        {
+            AdicionalCharge = adicionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdicionalCharge;
+        }
+    }
+}
---
+++ 05Heranca/Employee/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using PaymentEmployee.Entities;
+
+namespace PaymentEmployee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Enter the number of employess: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Outsourced (y/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+                if (ch == 'y')
+                {
+                    Console.Write("Adicional charge: ");
+                    double adicionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, adicionalCharge));
+                }
+                else if (ch == 'n')
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option.");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS:");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.Name + " - $" + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+        }
+    }
+}

c7d3857e65af00835427db056e81094f728264b3
Leticia-Mendes <mendesz.leticia@gmail.com>
2022-03-15 10:08:23 -0300

    exercicio poo

---
+++ 05POO/Tax/Entities/Company.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Tax.Entities
+{
+    class Company : TaxPayer
+    {
+        public int Employees { get; set; }
+
+        public Company() { }
+
+        public Company(string name, double anualIncome, int employes)
+            : base(name, anualIncome)
+        {
+            Employees = employes;
+        }
+
+        public override double Tax()
+        {
+            if (Employees > 10)
+            {
+                return AnualIncome * 0.14;
+            }
+            else
+            {
+                return AnualIncome * 0.16;
+            }
+        }
+    }
+}
---
+++ 05POO/Tax/Entities/TaxPayer.cs
@@ -0,0 +1,18 @@
+﻿namespace Tax.Entities
+{
+    abstract class TaxPayer
+    {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        public TaxPayer() { }
+
+        public TaxPayer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+
+        public abstract double Tax();
+    }
+}

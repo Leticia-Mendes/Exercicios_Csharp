125c806dbf0d5016e2733798881d7061f26127d0
Leticia-Mendes <mendesz.leticia@gmail.com>
2022-03-20 15:00:13 -0300

    exceptions

---
+++ 06Exception/Exception01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exception01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Number A: ");
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("Number B: ");
+                int b = int.Parse(Console.ReadLine());
+
+                int result = a / b;
+                Console.WriteLine(a + " / " + b + " = " + result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Error! Divide by zero is not allowed! ");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error! " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error! " + e.Message);
+            }
+        }
+    }
+}
